85553f697db5dd11eccf20f4e68294eab8708174
Ivan Mwesigwa <60838885+Ivanmwes@users.noreply.github.com>
2020-06-22 19:20:23 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+var circle = document.getElementbyId("circle");
+var upBtn = document.getElementbyId("upBtn");
+var downBtn = document.getElementbyId("downBtn");
+
+var rotateValue = circle.style.transform;
+var rotateSum;
+
+upBtn.onclick = function(){
+  rotateSum = rotateValue + "rotate(-90deg)";
+  circle.style.transform = rotateSum;
+  rotateValue = rotateSum;
+}
